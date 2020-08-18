1b3da2c1f207d885b9d8cfac0374820d3231dd05
charlie-browns <queen.jewels.def@gmail.com>
2020-08-19 00:22:12 +0900

    Add dist to copy it to the dir of django static files

---
+++ dist/service-worker.js
@@ -0,0 +1,29 @@
+importScripts("/precache-manifest.a6a39874e53cf73f4d8c791a25b06ada.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-undef,no-restricted-globals */
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.core.setCacheNameDetails({
+  prefix: "saleor-store-front",
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+  new RegExp("^http.*(?:png|gif|jpg|jpeg|webp|svg|csv|pdf)"),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [
+      new RegExp("/graphql"),
+      new RegExp("/dashboard"),
+      new RegExp("/media/export_files"),
+    ],
+  }
+);
+
