c255c1be7b2153fc67a600ac1ce66bc088b44b10
charlie-browns <queen.jewels.def@gmail.com>
2020-08-23 21:59:08 +0900

    delete dist

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/storefront-assets/precache-manifest.aef04bf05bffeafa87e503de3504452b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/storefront-assets/precache-manifest.819819ab272a95e5e0aa165e2b7eaf38.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef,no-restricted-globals */
 
